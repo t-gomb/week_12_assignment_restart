532812af505b08f562ddaddee9134bf2c662d9f7
t-gomb <trentgombas@gmail.com>
2021-04-11 14:34:30 -0700

    had_to_restart_fresh_was_workign_from_wrong_file_changed_layout_and_need_to_style_buttons

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+/* function one */
+/* function with arrays and links to text documents */
+/* function will use a for loop to loop through all the links */
+/* will save links to the text document */
+
+/* function two */
+/* make function(s) that open and close the desired pages */
+/* Eg. open up the note taking page or a saved note etc */
+/* need to figure this one out more *** */
+
+/* function three */
+/* make a switch(slider) to change between light and dark mode */
+/* do if have time or jsut make a light/dark button indivdually ooposed to slider. */
+/* use 'on click' to change the background color from the base(light) to a darker fitting theme. */
+/* change the text colors and nav bars/header/footer color to match the dark theme. */
